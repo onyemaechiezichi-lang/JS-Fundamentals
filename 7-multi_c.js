7ebb18d4d504f8ce153a30a72aedd445d546d10d
onyemaechiezichi-lang <onyemaechiezichi@gmail.com>
2025-09-19 17:50:47 +0100

    multi

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,19 +1,21 @@
-const { argv } = require('node:process');
+const { argv, exit } = require('node:process');
 
 let word = 'C is fun';
 let firstDigit = argv[2];
 
-// Check if there are enough arguments
-if (argv.length < 3) {
-    console.log('Missing number of occurrences');
-    process.exit(1); // Exit the process with a non-zero status
-}
+// print process.argv
+argv.forEach((val, index) => {
+    if(argv.length - 1 == index){
 
-// Check if the first argument is a number
-if (!isNaN(Number(firstDigit))) {
-    for (let i = 0; i < Number(firstDigit); i++) {
-        console.log(word);
-    }
-} else {
-    console.log('Missing number of occurrences');
-};
+        if(!isNaN(Number(firstDigit))){
+
+            for (let i = 0; i < firstDigit; i++) {
+            console.log(word);
+            }
+        }else{
+            console.log('Missing number of occurrences');
+        }
+
+
+    }   
+});
